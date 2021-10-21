e64387ab013687508b1bc85b80dff386f85615fa
Edilson Silva <2014342@aluno.univesp.br>
2021-10-21 17:24:11 -0300

    Adição do componente Header

---
+++ frontend/bancoalimentos/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div>
+            <div className="titulo">
+                <h1>Banco de Alimentos</h1>
+            </div>
+            <div className="ali-doados-24"></div>
+            <div className="ali-doados-in"></div>
+            <div className="ali-retiradas-24"></div>
+            <div className="ali-retiradas-in"></div>
+        </div>
+    )
+}
