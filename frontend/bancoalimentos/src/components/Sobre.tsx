58573f6e5bc365e41b5209d6f8a7a40267209b91
Edilson Silva <2014342@aluno.univesp.br>
2021-10-24 10:01:20 -0300

    Modificações na Página Sobre

---
+++ frontend/bancoalimentos/src/components/Sobre.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div>
+            <div className="titulo">
+                <h1>Minha Página Sobre</h1>
+            </div>
+            <div className="ali-doados-24">Informações Iniciais</div>
+            <div className="ali-doados-in"></div>
+            <div className="ali-retiradas-24"></div>
+            <div className="ali-retiradas-in"></div>
+        </div>
+    )
+}
