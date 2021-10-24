add0be754a114115860a8496dbb17f879290c557
DaniloIto <60575834+DaniloIto@users.noreply.github.com>
2021-10-24 10:22:04 -0300

    Alterado linha 12

--- frontend/bancoalimentos/src/components/Sobre.tsx
+++ frontend/bancoalimentos/src/components/Sobre.tsx
@@ -9,7 +9,7 @@ export default function Header(){
             <div className="ali-doados-24">Informações Iniciais</div>
             <div className="ali-doados-in">dados de bloco e apto</div>
             <div className="ali-retiradas-24"></div>
-            <div className="ali-retiradas-in"></div>
+            <div className="ali-retiradas-in">cadastro de alimentos retirados</div>
         </div>
     )
 }
