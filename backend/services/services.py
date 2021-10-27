b5995bc099aa687e40782cbd35cc4aa59f54c209
Edilson Silva <2014342@aluno.univesp.br>
2021-10-27 11:00:50 -0300

    Criação dos objetos produto e doacao. Criação dos serviços e entre point main.py

---
+++ backend/services/services.py
@@ -0,0 +1,44 @@
+from flask import json
+from flask.json import jsonify
+from flask import Flask, request
+from domain.doador import Doador
+
+from domain.produto import Produto
+
+def runApplication():
+    app = Flask(__name__)
+    app.config['DEBUG']=True
+
+    # Rotas para produtos---------------------------------------
+
+    @app.route('/api/v1/produto/listar',methods=['GET'])
+    def listarprodutos():
+        result = Produto()
+        return result.listarProdutos()
+
+    @app.route('/api/v1/produto/cadastro',methods=['POST'])
+    def cadastroproduto():
+        dados = request.get_json()
+        id = dados['idproduto']
+        nome = dados['nomeproduto']
+        cad = Produto(id,nome)
+        cad.cadastroProduto()
+        return "cadastrado com sucesso"
+    
+    # Rotas para doador ------------------------------------------
+
+    @app.route('/api/v1/doador/listar',methods=['GET'])
+    def listardoador():
+        result = Doador()
+        return result.listarDoador()
+
+    @app.route('/api/v1/doador/cadastro',methods=['POST'])
+    def cadastrodoador():
+        dados = request.get_json()
+        id = 0
+        bloco = dados['bloco_apartamento']
+        cad = Produto(id,bloco)
+        cad.cadastroDoador()
+        return "teste"
+
+    app.run()
