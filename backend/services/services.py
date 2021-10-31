4baddcff51e9237d2947d1f695699304310673c0
DaniloIto <60575834+DaniloIto@users.noreply.github.com>
2021-10-31 09:44:04 -0300

    alterado doacao.py e services.py - inserido rota doacao

--- backend/services/services.py
+++ backend/services/services.py
@@ -2,6 +2,7 @@ from flask import json
 from flask.json import jsonify
 from flask import Flask, request
 from domain.doador import Doador
+from domain.doacao import Doacao
 
 from domain.produto import Produto
 
@@ -35,10 +36,28 @@ def runApplication():
     @app.route('/api/v1/doador/cadastro',methods=['POST'])
     def cadastrodoador():
         dados = request.get_json()
+        produto = dados['idproduto']
+        doador = dados['iddoador']
+        qtde = dados['quantidade']
+        datadoacao = dados['datadoacao']
+        cad = Doacao(produto,doador,qtde,datadoacao)
+        cad.cadastroDoacao()
+        return "teste"
+
+# Rotas para doacao ------------------------------------------
+
+
+    @app.route('/api/v1/doacao/listar',methods=['GET'])
+    def listarDoacao():
+        result = Doacao()
+        return result.listarDoacao()
+
+    @app.route('/api/v1/doacao/cadastro',methods=['POST'])
+    def cadastroDoacao():
+        dados = request.get_json()
         id = 0
         bloco = dados['bloco_apartamento']
         cad = Produto(id,bloco)
         cad.cadastroDoador()
         return "teste"
-
     app.run()
