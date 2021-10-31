02ccf86f3fcab6cc3aedc09d27f73d78d7bd39fb
DaniloIto <60575834+DaniloIto@users.noreply.github.com>
2021-10-30 22:17:40 -0300

    alterado doacao.py

---
+++ backend/domain/doacao.py
@@ -0,0 +1,43 @@
+from flask.json import jsonify
+import traceback
+
+import pymysql
+
+class Doacao:
+    def __init__(self, idproduto=None, iddoador=None, quantidade=None, datadoacao=None):
+        self.idproduto = idproduto
+        self.iddoador = iddoador
+        self.quantidade = quantidade
+        self.datadoacao = datadoacao
+
+        self.conn = pymysql.connect(host='localhost',
+                            user='root',
+                            password='',
+                            database='bancoalimentos',
+                            charset='uft8mb4'
+                            cursorclass=pymysql.cursors.DictCursor)
+        
+    def cadastroDoacao(self):
+        try:
+            with self.conn.cursor() as cur:
+                sql = "INSERT INTO 'doacao' ('idproduto', 'iddoador', 'quantidade', 'datadoacao') VALUES(%s, %s, %i, %s)"
+                cur.execute(sql,(self.idproduto, self.iddoador, self.quantidade, self.datadoacao))
+                self.conn.commit()
+            except:
+                print("Erro ao tentar cadastrar os dados")
+                traceback.print_exc()
+            finally:
+                self.conn.close()
+    
+    def listarDoacao(self):
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute("SELECT * FROM doacao")
+                result = cur.fetchall()
+                return jsonify(result)
+        except:
+            print("Erro ao tentar listar doacoes")
+        finally:
+            self.conn.close()
+            
+        
